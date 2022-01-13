37b7d3ef976ad90ea4944a83b2420266e49043a8
achrafattary <attaryachraf.pro@gmail.com>
2022-01-13 16:16:19 +0100

    Two Fer

--- scala/two-fer/src/main/scala/Twofer.scala
+++ scala/two-fer/src/main/scala/Twofer.scala
@@ -1,3 +1,10 @@
 object Twofer {
-  def twofer(name: String): String = ???
+  def twofer(name: String = ""): String = {
+    if(name == ""){
+      "One for you, one for me."
+    }
+    else{
+      "One for "+name+", one for me."
+    }
+  }
 }
