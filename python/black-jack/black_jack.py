10952e66b1dc7ca46e3b8e2fef47e46b77acbfad
achrafattary <attaryachraf.pro@gmail.com>
2022-01-21 17:32:04 +0100

    black_jack exercice finished

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -5,6 +5,9 @@ How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
 """
 
 
+from pickle import FALSE
+
+
 def value_of_card(card):
     """Determine the scoring value of a card.
 
@@ -15,7 +18,13 @@ def value_of_card(card):
     if (card in face_cards): return 10
     elif(card == "A") : return 1
     else : return int(card)
-    
+def value_of_card_rectified(card_one,card_two) :
+    value_one = value_of_card(card_one)
+    value_two = value_of_card(card_two)
+    if(card_one == "A") : value_one = 11
+    if (card_two == "A") : value_two = 11
+    return value_one,value_two
+
 
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
@@ -35,9 +44,9 @@ def higher_card(card_one, card_two):
     elif (card_two == "A") : b = 1
     else : b = int(card_two)
     #returning the biggest value
-    if (a > b ) : return a
-    elif (a < b) : return b
-    else : return a,b
+    if (a > b ) : return card_one
+    elif (a < b) : return card_two
+    else : return card_one,card_two
     # I should have used the first function instead of doing it again with if conditionals
 
 
@@ -47,10 +56,10 @@ def value_of_ace(card_one, card_two):
     :param card_one, card_two: str - card (J, Q, K == 10, numerical value otherwise)
     :return: int - value of the upcoming ace card (either 1 or 11).
     """
-    value_one = value_of_card(card_one)
-    value_two = value_of_card(card_two)
+    value_one,value_two = value_of_card_rectified(card_one,card_two)
+    
 
-    if(value_one +  value_two + 11 < 21) : return 1
+    if(value_one +  value_two + 11 > 21) : return 1
     else  : return 11
     
 
@@ -61,8 +70,9 @@ def is_blackjack(card_one, card_two):
     :param card_one, card_two: str - cards dealt.  J, Q, K = 10, 'A' = 11, all others are numerical value.
     :return: bool - if the hand is a blackjack (two cards worth 21).
     """
-
-    pass
+    value_one,value_two = value_of_card_rectified(card_one,card_two)
+    if(value_two + value_one == 21) : return True
+    else : return False
 
 
 def can_split_pairs(card_one, card_two):
@@ -71,8 +81,9 @@ def can_split_pairs(card_one, card_two):
     :param card_one, card_two: str - cards in hand.
     :return: bool - if the hand can be split into two pairs (i.e. cards are of the same value).
     """
-
-    pass
+    value_one,value_two = value_of_card_rectified(card_one,card_two)
+    if(value_one == value_two) : return True
+    else : return False
 
 
 def can_double_down(card_one, card_two):
@@ -81,5 +92,8 @@ def can_double_down(card_one, card_two):
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - if the hand can be doubled down (i.e. totals 9, 10 or 11 points).
     """
-
-    pass
+    value_one = value_of_card(card_one)
+    value_two = value_of_card(card_two)
+    double_down = [9,10,11]
+    if (value_one + value_two in double_down) : return True
+    else : return False
