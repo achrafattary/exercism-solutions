e5b7ab183f3d721c63f38a949369f4d8a6b25686
achrafattary <attaryachraf.pro@gmail.com>
2022-01-25 22:55:19 +0100

    better solution for two_fer python

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,9 +1,6 @@
 def two_fer(name = "you"):
     txt = "One for {fname}, one for me."
-    try : 
-        if name == "you" :
-            return txt.format(fname = "you")
-        else :
-            return txt.format(fname = name)
+    try :
+        return txt.format(fname = name)
     except ValueError as e :
-        raise ValueError("input not string") from e 
+        raise ValueError("input not string")
