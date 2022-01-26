9f75b50d450274ae00849f2ae72ecf642bb62f4d
achrafattary <attaryachraf.pro@gmail.com>
2022-01-26 23:19:57 +0100

    commit test

--- python/tisbury-treasure-hunt/tuples.py
+++ python/tisbury-treasure-hunt/tuples.py
@@ -1,12 +1,32 @@
+'''
+Tuples practice exercise frfom the python track
+'''
+
 def get_coordinate(record):
+    """
+
+    :param record: tuple - a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
     return record[1]
 
 
 def convert_coordinate(coordinate):
-    return (coordinate[0],coordinate[1])
+    """
+
+    :param coordinate: str - a string map coordinate
+    :return:  tuple - the string coordinate seperated into its individual components.
+    """
+    return (coordinate[0], coordinate[1])
 
 
 def compare_records(azara_record, rui_record):
+    """
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: bool - True if coordinates match, False otherwise.
+    """
     return convert_coordinate(azara_record[1]) == rui_record[1]
 
 
@@ -19,17 +39,16 @@ def create_record(azara_record, rui_record):
     """
     if compare_records(azara_record,rui_record):
         return  azara_record + rui_record
-    else:
-        return "not a match"
+    return "not a match"
 
 
 def clean_up(combined_record_group):
     """
-
     :param combined_record_group: tuple of tuples - everything from both participants.
     :return: string of tuples separated by newlines - everything "cleaned". Excess coordinates and information removed.
     """
-    txt = ""
-    for e in combined_record_group:
-        txt = "".join((txt,str((e[0],) + e[2:]), "\n"))
+    txt = []
+    for item in combined_record_group:
+        txt.append(str((item[0],) + item[2:]))
+    txt = "\n".join(txt) + "\n"
     return txt
