3b11a12c2ef071ea4b95f4333adbb3e9aa1e4682
achrafattary <attaryachraf.pro@gmail.com>
2022-01-29 21:43:19 +0100

    inventory management exercice python track

---
+++ python/inventory-management/dicts.py
@@ -0,0 +1,39 @@
+def create_inventory(items):
+    dictionary = {}
+    for e in set(items):
+        dictionary[e] = items.count(e)
+    return dictionary
+
+
+def add_items(inventory, items):
+    dic = create_inventory(items)
+    x = list(inventory.items())
+    for e in x :
+        if e[0] in dic :
+            dic[e[0]] = dic[e[0]] + e[1]
+        else:
+            dic[e[0]] = e[1]
+    return dic
+
+
+def decrement_items(inventory, items):
+    for e in items:
+        if (inventory[e] >= 1):
+            inventory[e] -= 1
+    return inventory
+
+
+def remove_item(inventory, item):
+    try :
+        inventory.pop(item)
+        return inventory
+    except:
+        return inventory
+    
+
+def list_inventory(inventory):
+    lst = list(inventory.items())
+    for index,item in enumerate(lst):
+        if item[1] == 0:
+            lst.remove(item)
+    return lst
